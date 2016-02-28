85caac7f3119f3061e09cecaf12101e2f2c4aaf4
Nathan Stien <nathanism@gmail.com>
2016-02-28 17:21:18 -0600

    messed around with simple resources and added a websocket chat server
    i just took the example haskell chatserver lhs file and wrapped it into
    a Wai.Application so that i could use it as a Raw endpoint in servant.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,26 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( startApp
     ) where
 
+import Control.Concurrent (MVar, newMVar)
+import Control.Monad.Trans.Either
+
 import Data.Aeson
 import Data.Aeson.TH
+import Data.List (find)
+import Data.Maybe (fromJust)
+
 import Network.Wai
 import Network.Wai.Handler.Warp
+
 import Servant
+
+import qualified ChatServer as Chat
 
 data User = User
   { userId        :: Int
@@ -19,21 +30,42 @@ data User = User
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "users" :> Get '[JSON] [User]
+type API = "users" :> QueryParam "firstName" String
+                   :> QueryParam "lastName" String
+                   :> Get '[JSON] [User]
+    :<|> "user" :> Capture "userid" Int
+                :> Get '[JSON] User
+    :<|> "chat" :> Raw
 
 startApp :: IO ()
-startApp = run 8080 app
-
-app :: Application
-app = serve api server
+startApp = do
+    chatState <- newMVar Chat.newServerState
+    run 8080 $
+        serve api (server chatState)
 
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
+server :: MVar Chat.ServerState -> Server API
+server chatState =
+         getUsers
+    :<|> getUser
+    :<|> Chat.waiApp chatState
 
 users :: [User]
 users = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
+        , User 3 "Agent" "Einstein"
+        , User 4 "Fig" "Newton"
         ]
+
+getUsers mfirst mlast = return $ filter (\u -> makePred mfirst userFirstName u
+                                               && makePred mlast userLastName u)
+                                        users
+  where makePred needle field = maybe (const True) (\s -> (== s) . field) needle
+
+
+getUser uid = case find ((== uid) . userId) users of
+    Just u -> return u
+    Nothing -> left err404 { errBody = "Dave's not here, man." }
+
