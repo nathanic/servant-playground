c2e02b4ee9887f6593b5174b7382a3220d8f6ddd
Nathan Stien <nathanism@gmail.com>
2016-02-28 22:00:57 -0600

    add a li'l POST handler (and make the user list dynamic)

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,13 +2,15 @@
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeOperators     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module Lib
     ( startApp
     ) where
 
-import Control.Concurrent (MVar, newMVar, modifyMVar)
+import Control.Concurrent (MVar, newMVar, modifyMVar, modifyMVar_, readMVar)
 import Control.Monad (join)
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Either
 
 import Data.Aeson
@@ -17,6 +19,7 @@ import Data.List (find)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (fromJust, fromMaybe)
+import Data.Monoid ((<>))
 
 import Network.Wai
 import Network.Wai.Handler.Warp
@@ -25,7 +28,11 @@ import Servant
 
 import qualified ChatServer as Chat
 
-type AppState = MVar (Map String (MVar Chat.ServerState))
+data State = State { chatStates :: Map String (MVar Chat.ServerState)
+                   , users :: [User]
+                   }
+
+type AppState = MVar State
 
 data User = User
   { userId        :: Int
@@ -40,47 +47,67 @@ type API = "users" :> QueryParam "firstName" String
                    :> Get '[JSON] [User]
     :<|> "user" :> Capture "userid" Int
                 :> Get '[JSON] User
+    :<|> "users" :> ReqBody '[JSON] User
+                 :> Post '[JSON] User
     :<|> "chat" :> Capture "chatroom" String
                 :> Raw
 
 startApp :: IO ()
 startApp = do
-    chatStates <- newMVar mempty
+    stateRef <- newMVar $ State mempty initialUsers
     run 8080 $
-        serve api (server chatStates)
+        serve api (server stateRef)
 
 api :: Proxy API
 api = Proxy
 
+-- TODO: probably switch to embedding this crap in a monad
 server :: AppState -> Server API
-server chatStates =
-         getUsers
-    :<|> getUser
-    :<|> getChatroom chatStates
+server stateRef =
+         getUsers stateRef
+    :<|> getUser stateRef
+    :<|> makeUser stateRef
+    :<|> getChatroom stateRef
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        , User 3 "Agent" "Einstein"
-        , User 4 "Fig" "Newton"
-        ]
+initialUsers :: [User]
+initialUsers =
+    [ User 1 "Isaac" "Newton"
+    , User 2 "Albert" "Einstein"
+    , User 3 "Agent" "Einstein"
+    , User 4 "Fig" "Newton"
+    ]
 
-getUsers mfirst mlast = return $ filter (\u -> makePred mfirst userFirstName u
-                                               && makePred mlast userLastName u)
-                                        users
+getUsers stateRef mfirst mlast = do
+    State{..} <- liftIO $ readMVar stateRef
+    return $ filter (\u -> makePred mfirst userFirstName u
+                           && makePred mlast userLastName u)
+                    users
   where makePred needle field = maybe (const True) (\s -> (== s) . field) needle
 
-getUser uid = case find ((== uid) . userId) users of
-    Just u -> return u
-    Nothing -> left err404 { errBody = "Dave's not here, man." }
+getUser stateRef uid = do
+    State{..} <- liftIO $ readMVar stateRef
+    case find ((== uid) . userId) users of
+        Just u  -> return u
+        Nothing -> left err404 { errBody = "Dave's not here, man." }
 
+makeUser stateRef user = do
+    liftIO $ modifyMVar_ stateRef $
+        \st@State{..} -> return st { users = user : users }
+    return user
+
+-- I'm exploiting the fact that Application is an alias for
+--   Request → (Response → IO ResponseReceived) → IO ResponseReceived
+-- grabbing its params so that I can steal its monad for my MVar effects
 getChatroom :: AppState -> String -> Application
-getChatroom chatStates chatroom req respond = do
-    state <- modifyMVar chatStates $ \states ->
-        case Map.lookup chatroom states of
-            Just state -> return (states, state)
-            Nothing    -> do
-                state <- newMVar Chat.newServerState
-                return (Map.insert chatroom state states, state)
-    Chat.waiApp state req respond
+getChatroom stateRef chatroom req respond = do
+    State{..} <- readMVar stateRef
+    roomState <- modifyMVar stateRef $ \st@State{..} ->
+        case Map.lookup chatroom chatStates of
+            Just roomState -> return (st, roomState)
+            Nothing        -> do
+                roomState <- newMVar Chat.newServerState
+                let chatStates' = Map.insert chatroom roomState chatStates
+                return (st { chatStates = chatStates'}, roomState)
+    Chat.waiApp roomState req respond
+
 
